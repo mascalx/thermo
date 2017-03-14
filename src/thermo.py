39585947d98943717e7b6561b10022b3e73bd7b1
Mascal <carlomascellani@tiscali.it>
2017-03-14 09:02:56 +0100

    Update thermo.py

--- src/thermo.py
+++ src/thermo.py
@@ -285,11 +285,11 @@ def GetButton():
     
 #** Show main menu
 def ShowMenu1(display): 
+    draw = ImageDraw.Draw(display)
     draw.rectangle([(0,54),(320,199)], fill = WHITE) # Clear the status bar
     Write(display,font_medium,"1.Programma orario",(15,70),BLACK)
     Write(display,font_medium,"2.Vacanza",(15,100),BLACK)
     Write(display,font_medium,"3.Opzioni",(15,130),BLACK)
-    draw = ImageDraw.Draw(display)
     del draw
     TFT_Display()
     
